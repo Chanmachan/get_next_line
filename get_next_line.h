a9439914826b42f468370395dbe8a07a744360ed
chanma <yunosuke.homma@icloud.com>
2022-06-21 19:35:44 +0900

    add include & prototype

--- get_next_line.h
+++ get_next_line.h
@@ -5,4 +5,8 @@
 #ifndef GET_NEXT_LINE_GET_NEXT_LINE_H
 #define GET_NEXT_LINE_GET_NEXT_LINE_H
 
+#include <stdlib.h>
+#include <unistd.h>
+char	*get_next_line(int fd);
+
 #endif //GET_NEXT_LINE_GET_NEXT_LINE_H
