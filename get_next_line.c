dd9ea400748a063a961f00d9d2fd6ed0540155d6
chanma <yunosuke.homma@icloud.com>
2022-06-25 22:25:46 +0900

    add get_one_line func

--- get_next_line.c
+++ get_next_line.c
@@ -29,6 +29,28 @@ char	*read_line(char *str, int fd)
 	return (str);
 }
 
+char	*get_one_line(char *str)
+{
+	size_t 	i;
+	char	*rtn_str;
+
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (str[i] != '\n' && str[i] != '\0')
+	{
+		i++;
+	}
+	rtn_str = (char *) malloc(sizeof(char) * (i + 1));
+	ft_strlcpy(rtn_str, str, i);
+	return (rtn_str);
+}
+
+/*char	*delete_last_line(char *str, fd)
+{
+
+}*/
+
 char	*get_next_line(int fd)
 {
 	static char *str;
@@ -36,8 +58,11 @@ char	*get_next_line(int fd)
 
 	//strにread_line関数を作って読み込む
 	str = read_line(str, fd);
-	//strの中身をrtn_strにうつす
-	rtn_str = ft_strdup(str);
+	printf("str : \n%s\n-----------", str);
+	//strの一行目をrtn_strにうつす
+	rtn_str = get_one_line(str);
+	//もういらない部分を取り除く
+//	str = delete_last_line(str, fd);
 	return (rtn_str);
 }
 
