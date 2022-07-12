9318810c7977f40372fc0c454c9d69ae7aea5582
chanma <yunosuke.homma@icloud.com>
2022-07-12 15:46:54 +0900

    delete some func

--- get_next_line.h
+++ get_next_line.h
@@ -23,9 +23,6 @@
 
 char	*get_next_line(int fd);
 size_t	ft_strlen(const char *str);
-char	*ft_strchr(char *str, int c);
-size_t	ft_strlcpy(char *dest, const char *src, size_t n);
-size_t	ft_strlcat(char *dst, const char *src, size_t size);
 char	*ft_strjoin(char *s1, char *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 
